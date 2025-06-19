43eb1d91a4c3794568f266d48c95f29416ee3d81
ryusei2790 <ryu.chn1978@gmail.com>
2025-06-19 10:24:36 +0900

    backgroundと初回レンダリングの実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,27 @@ import Image from "next/image";
 import GalaxyBackground from "./components/layouts/Background";
 import Head from './components/Head/Head'
 
+
 export default function Home() {
+  const [fadeOut, setFadeOut] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
+  const handleGalaxyReady = () => {
+    setFadeOut(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 800);
+  };
   
   
   return (
     <>
-      <GalaxyBackground />
+      {isLoading && (
+        <div className={`splash=screen${fadeOut ? ' fade-out': ''}`}>
+          <img src="/images_load_background.png" alt="Loading..." style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
+        </div>
+      )}
+      <GalaxyBackground onReady={handleGalaxyReady} />
       <main className="">
 
       </main>
