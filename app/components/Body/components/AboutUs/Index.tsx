2cac7e80882c14a2ad5cfd0aa4c7c98a9616f419
ryusei2790 <ryu.chn1978@gmail.com>
2025-07-06 12:32:20 +0900

    Update BodyAbout and Index components to enhance AboutUs content
    - Modified BodyAbout component to include additional information such as vision, mission, and project descriptions.
    - Updated Index component to accept new props for vision, mission, and project details, improving the overall content structure and presentation on the AboutUs page.

--- app/components/Body/components/AboutUs/Index.tsx
+++ app/components/Body/components/AboutUs/Index.tsx
@@ -5,12 +5,17 @@ import Image from 'next/image';
 
 type IndexProps = {
   h2: string;
-  description: string;
+  vision: string;
+  mission: string;
+  descriptionService: string;
+  contentsIT: string;
+  descriptionNewProject: string;
+  contentsNewProject: string;
   button: string;
   link: string;
 };
 
-const Index: React.FC<IndexProps> = ({ h2, description, button, link }) => {
+const Index: React.FC<IndexProps> = ({ h2, vision, mission, descriptionService, contentsIT, descriptionNewProject, contentsNewProject, button, link }) => {
   return (
     <>
       <h2>{h2}</h2>
@@ -19,7 +24,12 @@ const Index: React.FC<IndexProps> = ({ h2, description, button, link }) => {
           <div className={styles.image}>
           <Image src="/images/masaki.jpg" alt="代表" className={styles.zenovithImage} width={100} height={100} />
           </div>
-          <p>{description}</p>
+          <h3>{vision}</h3>
+          <h4>{mission}</h4>
+          <h5>{descriptionService}</h5>
+          <p>{contentsIT}</p>
+          <h5>{descriptionNewProject}</h5>
+          <p>{contentsNewProject}</p>
         </button>
       </a>
     </>
